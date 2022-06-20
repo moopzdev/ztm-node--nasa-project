31aea6e150ade21359c8ebc85107648409bc7e5e
Moopz-Dev <pongzthor@gmail.com>
2022-06-20 14:41:12 +0700

    feat: adds database and planet model

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,36 @@
+const { parse } = require("csv-parse")
+const path = require("path")
+const fs = require("fs")
+
+const habitablePlanets = [];
+
+isHabitable = (planet) => {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11
+    && planet["koi_prad"] < 1.6
+}
+
+const loadPlanetsData = () => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, "..", "..", "data", "kepler_data.csv"))
+      .pipe(parse({
+        comment: "#",
+        columns: true,
+      }))
+      .on('data', (data) => {
+        if (isHabitable(data)) {
+          habitablePlanets.push(data)
+        }
+      }).on('error', err => {
+        reject(err)
+      })
+      .on('end', () => {
+        resolve()
+      });
+  })
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets
+}
