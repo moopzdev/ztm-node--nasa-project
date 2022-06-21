f25cc667dce62101a889babe6c35d22692d86e8f
Moopz-Dev <pongzthor@gmail.com>
2022-06-21 19:10:53 +0700

    feat : adds tests for launches endpoints

---
+++ server/src/models/launches.test.js
@@ -0,0 +1,59 @@
+const request = require("supertest")
+const app = require("../app")
+
+describe("Test GET/launches", () => {
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200)
+  })
+})
+
+describe("Test POST/launches", () => {
+  const completeLaunchData = {
+    mission: "Bring Back Kaguya",
+    rocket: "Greenwood",
+    target: "Kepler-186 f",
+    launchDate: "January 4,2028",
+  }
+  const LaunchDataWithoutDate = {
+    mission: "Bring Back Kaguya",
+    rocket: "Greenwood",
+    target: "Kepler-186 f",
+  }
+  const LaunchDataWithInvalidDate = {
+    mission: "Bring Back Kaguya",
+    rocket: "Greenwood",
+    target: "Kepler-186 f",
+    launchDate: "Bloopers",
+  }
+  test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(completeLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201)
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf()
+    const responseDate = new Date(response.body.launchDate).valueOf()
+    expect(responseDate).toBe(requestDate)
+    expect(response.body).toMatchObject(LaunchDataWithoutDate)
+  })
+  test("It should catch missing required properties", async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(LaunchDataWithoutDate)
+      .expect('Content-Type', /json/)
+      .expect(400)
+    expect(response.body).toStrictEqual({ error: "Some required inputs missing." })
+  })
+  test("It should catch invalid dates", async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(LaunchDataWithInvalidDate)
+      .expect('Content-Type', /json/)
+      .expect(400)
+    expect(response.body).toStrictEqual({ error: "Invalid launch date" })
+  })
+})
