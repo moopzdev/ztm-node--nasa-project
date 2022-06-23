361e231fd0ef2dade610eb34ba61240d3464fd3c
Moopz-Dev <pongzthor@gmail.com>
2022-06-23 17:02:34 +0700

    feat: implements MongoDB Atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http")
+const mongoose = require("mongoose")
 
 const app = require('./app')
 
@@ -6,9 +7,18 @@ const { loadPlanetsData } = require("./models/planets.model")
 
 const PORT = process.env.PORT || 8000
 
+const MONGO_URL = "mongodb+srv://nasa-api:Jbq62vfTDCmK3XSp@nasacluster.cxqb3hy.mongodb.net/?retryWrites=true&w=majority"
+
 const server = http.createServer(app)
 
+mongoose.connection.once('open', () => {
+  console.log("MongoDB connection ready")
+})
+
+mongoose.connection.on('error', (err) => { console.log(err) })
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL)
   await loadPlanetsData()
 
   server.listen(PORT, () => {
